f30f32b20b5213b5aeb321f6c125532ac7707c5f
Rehman Ahmadzai <github@ahmadz.ai>
2022-02-09 00:02:36 -0500

    added prefix-based command and statement handler

--- src/cqlite.c
+++ src/cqlite.c
@@ -2,23 +2,50 @@
 #include <string.h>
 #include "REPL/InputBuffer.h"
 #include "REPL/repl.h"
+#include "statements/metaCommand.h"
+#include "statements/statement.h"
+
+const char META_COMMAND_PREFIX='.';
 
 int main() {
     InputBuffer *input_buffer = new_input_buffer();
     printf("CQLite - Version 0.0.1\n");
 
     //REPL
-    while (1) {
+    while (1)
+    {
         print_prompt();
         read_input(input_buffer);
 
-        if ( strcmp(input_buffer->buffer, ".exit") == 0){
-            close_input(input_buffer);
-            exit(EXIT_FAILURE);
+        if ((input_buffer->buffer[0] == META_COMMAND_PREFIX))
+        {
+            strip_prefix(input_buffer, META_COMMAND_PREFIX);
+            switch (handle_meta_command(input_buffer))
+            {
+                case SUCCESS_META_COMMAND:
+                {
+                    continue;
+                }
+                case UNRECOGNIZED_META_COMMAND:
+                {
+                    printf("Unrecognized command: %s\n", input_buffer->buffer);
+                    continue;
+                }
+            }
         }
-        else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
+        {
+            case SUCCESS_PREPARE_STATEMENT:
+                break;
+            case UNRECOGNIZED_PREPARE_STATEMENT:
+                printf("Unrecognized statement: %s\n", input_buffer->buffer);
+                continue;
         }
+
+        execute_statement(&statement);
+        printf("Executed: %s", input_buffer->buffer);
     }
 }
 
